965b59b6ca979ab6709b1e40330f064f30013db9
Emmanuel Mendoza <emmanuelmendoza448@gmail.com>
2025-05-10 21:36:29 -0600

    se empieza a separar en capas

--- MiTablaRepositorio.cs
+++ MiTablaRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +7,73 @@ using System.Threading.Tasks;
 
 namespace Conectividad001
 {
-    public class MiTablaRepositorio
+    public class GuardarDatos
     {
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public bool Activo { get; set; }
-        public string Descripcion { get; set; }
+        public string Errores { get; set; }
 
-        public MiTablaRepositorio(string Nombre, int Edad, bool Activo, string Descripcion = null)
+        public GuardarDatos(string Nombre, int Edad, bool Activo, string Errores = null)
         {
             this.Nombre = Nombre;
             this.Edad = Edad;
             this.Activo = Activo;
-            this.Descripcion = Descripcion;
+            this.Errores = Errores;
+        }
+    }
+
+    public class miTablaRepositorio
+    {
+        public List<GuardarDatos> Seleccionar(string datosBD, string instruccion, SqlParameter[] parametros)
+        {
+            string accion = "Seleccionar";
+
+            var resultados = new List<GuardarDatos>();
+            try
+            {
+                //se establece la conexión con la BD
+                using (SqlConnection conexion = new SqlConnection(datosBD))
+                {
+                    conexion.Open();
+
+                    //se manda el comando
+                    using (SqlCommand comando = new SqlCommand(instruccion, conexion))
+                    {
+                        //se cargan los parámetros
+                        comando.Parameters.AddRange(parametros);
+
+                        using (SqlDataReader reader = comando.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                int contar = 1;
+                                while (reader.Read())
+                                {
+                                    //se optienen los datos
+                                    string nombre = reader.IsDBNull(1) ? "vacío" : reader.GetString(1);
+                                    int edad = reader.IsDBNull(2) ? 0 : reader.GetInt32(2);
+                                    bool activo = reader.IsDBNull(3) ? false : reader.GetBoolean(3);
+
+                                    resultados.Add(new GuardarDatos(nombre, edad, activo));
+                                }
+                                return resultados;
+                            }
+                            else
+                            {
+                                resultados.Add(new GuardarDatos("", 0, false, $"No se encontraron datos al {accion}"));
+                                return resultados;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultados.Add(new GuardarDatos("", 0, false, $"Error al {accion}: {ex.Message}"));
+
+                return resultados;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,84 +11,105 @@ namespace Conectividad
         public static void Main(string [] args)
         {
             string datosBD = "Server=.;Database=miBD;Trusted_Connection=true;TrustServerCertificate=true;";
-            Console.WriteLine("e0001");
-            
-            //Seleccionar(datosBD);
 
-            var resultados = Seleccionar(datosBD);
-            int contar = 1;
-            foreach (var item in resultados)
+            Seleccionar(datosBD);
+        }
+
+        private static void Seleccionar(string datosBD)
+        {
+            //instruccion
+            string instruccion = "SELECT * FROM miTabla WHERE id IN (@id01, @id02, @id03)";
+
+            //datos a pasar
+            SqlParameter[] parametros = new SqlParameter[]
             {
-                if (item.Descripcion == null)
+                new SqlParameter("@id01", 1),
+                new SqlParameter("@id02", 2),
+                new SqlParameter("@id03", 3)
+            };
+
+            //creo el objeto
+            var repo = new miTablaRepositorio();
+
+            //y selecciono el metodo de este objeto
+            var consultaSeleccionar = repo.Seleccionar(datosBD, instruccion, parametros);
+
+            int contar = 1;
+            foreach (var item in consultaSeleccionar)
+            {
+                if (item.Errores == null)
                 {
                     //sin descripción
                     Console.WriteLine($"\n-------------------- \n El dato número {contar++} es:\n Nombre: {item.Nombre},\n Edad: {item.Edad},\n Activo: {item.Activo}");
                 }
                 else
                 {
-                    Console.WriteLine("!!!" + item.Descripcion);
+                    //para contener los errores
+                    Console.WriteLine("!!!" + item.Errores);
                 }
             }
+
         }
 
 
-        private static List<MiTablaRepositorio> Seleccionar(string datosBD)
-        {
-            accion = "Seleccionar";
-            var resultados = new List<MiTablaRepositorio>();
-            Console.WriteLine("e0002");
-            try
-            {
-                //se establece la conexión con la BD
-                using (SqlConnection conexion = new SqlConnection(datosBD))
-                {
-                    string instruccion = "SELECT * FROM miTabla WHERE id IN (@id01, @id02, @id03)";
-                    conexion.Open();
 
-                    //se manda el comando
-                    using (SqlCommand comando = new SqlCommand(instruccion, conexion))
-                    {
-                        //se cargan los parámetros
-                        comando.Parameters.AddWithValue("@id01", 1);
-                        comando.Parameters.AddWithValue("@id02", 2);
-                        comando.Parameters.AddWithValue("@id03", 3);
+//private static List<MiTablaRepositorio> Seleccionar2(string datosBD)
+//        {
+//            accion = "Seleccionar";
+//            var resultados = new List<MiTablaRepositorio>();
+            
+//            try
+//            {
+//                //se establece la conexión con la BD
+//                using (SqlConnection conexion = new SqlConnection(datosBD))
+//                {
+//                    string instruccion = "SELECT * FROM miTabla WHERE id IN (@id01, @id02, @id03)";
+//                    conexion.Open();
 
-                        using (SqlDataReader reader = comando.ExecuteReader())
-                        {
-                            //creo que hay errro en la segunda parte
-                            if (reader.HasRows)
-                            {
-                                int contar = 1;
-                                while (reader.Read())
-                                {
-                                    string nombre = reader.IsDBNull(1) ? "vacío" : reader.GetString(1);
-                                    int edad = reader.IsDBNull(2) ? 0 : reader.GetInt32(2);
-                                    bool activo = reader.IsDBNull(3) ? false : reader.GetBoolean(3);
+//                    //se manda el comando
+//                    using (SqlCommand comando = new SqlCommand(instruccion, conexion))
+//                    {
+//                        //se cargan los parámetros
+//                        comando.Parameters.AddWithValue("@id01", 1);
+//                        comando.Parameters.AddWithValue("@id02", 2);
+//                        comando.Parameters.AddWithValue("@id03", 3);
 
-                                    //Console.WriteLine($"\n-------------------- \n El dato número {contar++} es:\n Nombre: {nombre},\n Edad: {edad},\n Activo: {activo}");
+//                        using (SqlDataReader reader = comando.ExecuteReader())
+//                        {
+//                            //creo que hay errro en la segunda parte
+//                            if (reader.HasRows)
+//                            {
+//                                int contar = 1;
+//                                while (reader.Read())
+//                                {
+//                                    string nombre = reader.IsDBNull(1) ? "vacío" : reader.GetString(1);
+//                                    int edad = reader.IsDBNull(2) ? 0 : reader.GetInt32(2);
+//                                    bool activo = reader.IsDBNull(3) ? false : reader.GetBoolean(3);
 
-                                    resultados.Add(new MiTablaRepositorio(nombre, edad, activo));
-                                }
-                                return resultados;
-                            }
-                            else
-                            {
-                                //Console.WriteLine($"No se encontraron datos al {accion}");
-                                resultados.Add(new MiTablaRepositorio("", 0, false, $"No se encontraron datos al {accion}"));
-                                return resultados;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //Console.WriteLine($"Error al {accion}: {ex.Message}");
-                resultados.Add(new MiTablaRepositorio("", 0, false, $"Error al {accion}: {ex.Message}"));
+//                                    //Console.WriteLine($"\n-------------------- \n El dato número {contar++} es:\n Nombre: {nombre},\n Edad: {edad},\n Activo: {activo}");
 
-                return resultados;
-            }
-        }
+//                                    resultados.Add(new MiTablaRepositorio(nombre, edad, activo));
+//                                }
+//                                return resultados;
+//                            }
+//                            else
+//                            {
+//                                //Console.WriteLine($"No se encontraron datos al {accion}");
+//                                resultados.Add(new MiTablaRepositorio("", 0, false, $"No se encontraron datos al {accion}"));
+//                                return resultados;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                //Console.WriteLine($"Error al {accion}: {ex.Message}");
+//                resultados.Add(new MiTablaRepositorio("", 0, false, $"Error al {accion}: {ex.Message}"));
+
+//                return resultados;
+//            }
+//        }
 
         private static void Insertar(string datosBD)
         {
